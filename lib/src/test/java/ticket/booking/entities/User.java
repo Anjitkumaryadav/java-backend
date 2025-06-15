3f46af2c9934ea4ade249a13f6f76c491dcc2395
Anjitkumaryadav <anjitaniket@gmail.com>
2025-06-15 18:12:31 +0530

    basic setup

---
+++ lib/src/test/java/ticket/booking/entities/User.java
@@ -0,0 +1,13 @@
+package ticket.booking.entities;
+
+public class User {
+   private String name;
+
+   private String password;
+
+   private String hashPassword;
+
+   private List <ticket> ticketBooking;
+
+   private String userId;
+}
